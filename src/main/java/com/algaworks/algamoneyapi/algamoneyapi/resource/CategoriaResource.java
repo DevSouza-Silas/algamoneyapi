1dd444a2c94a6d706d8bf8c64dd0f9120dfcf539
DevSouza-Silas <sillassouza.ads@gmail.com>
2024-05-28 12:43:33 -0300

    Tratamento de erros com mensagens customizadas

--- src/main/java/com/algaworks/algamoneyapi/algamoneyapi/resource/CategoriaResource.java
+++ src/main/java/com/algaworks/algamoneyapi/algamoneyapi/resource/CategoriaResource.java
@@ -1,5 +1,6 @@
 package com.algaworks.algamoneyapi.algamoneyapi.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +13,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algaworks.algamoneyapi.algamoneyapi.model.Categoria;
 import com.algaworks.algamoneyapi.algamoneyapi.repository.CategoriaRepository;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/categorias")
@@ -30,8 +34,13 @@ public class CategoriaResource {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public void criar(@RequestBody Categoria categoria) {
-		repository.save(categoria);
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = repository.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		return ResponseEntity.created(uri).body(categoriaSalva);
 	}
 	
 	@GetMapping("/{codigo}")
